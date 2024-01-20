bbdb41b6dc963c62539b87cd68a3041894b09119
israel meir <144207231+israel-meir@users.noreply.github.com>
2024-01-20 23:32:07 +0200

    Add files via upload

---
+++ AuthForge/src/main/java/com/authforge/web/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.authforge.web.controller;
+
+import com.authforge.service.AuthForgeService;
+import com.authforge.web.dto.LoginRequest;
+import com.authforge.web.dto.SignUpRequest;
+import com.authforge.web.dto.UserDto;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.security.auth.message.AuthException;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthForgeService authForgeService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<Void> signUp(@RequestBody SignUpRequest signUpRequest) {
+        if (authForgeService.signUp(signUpRequest.getUsername(), signUpRequest.getPassword())) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest)
+            throws AuthException {
+        String token = authForgeService.authenticateAndGenerateToken(loginRequest.getUsername(),
+                                                                     loginRequest.getPassword());
+        return ResponseEntity.ok(token);
+    }
+
+    @GetMapping("/parse-token")
+    public ResponseEntity<UserDto> parseToken(@RequestHeader("Authorization") String token) {
+        UserDto userDto = authForgeService.parseTokenAndGetUser(token);
+        return ResponseEntity.ok(userDto);
+    }
+}
