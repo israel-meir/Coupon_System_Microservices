0d41ca076619b12e82db280107a31c1f422ba7c5
israel meir <144207231+israel-meir@users.noreply.github.com>
2024-01-20 23:33:30 +0200

    Add files via upload

---
+++ Archimedes/src/test/java/com/israel/coupon/archimedes/ArchimedesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israel.coupon.archimedes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchimedesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
